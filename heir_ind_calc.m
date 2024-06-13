161e95805d4d51a5ea89a43806971bbff17499ef
anandpathak31 <57100437+anandpathak31@users.noreply.github.com>
2024-06-13 13:31:31 -0400

    Update heir_ind_calc.m

--- heir_ind_calc.m
+++ heir_ind_calc.m
@@ -1,4 +1,4 @@
-function heir_ind = heir_ind_calc(Q_mat,N,layers)
+function hier_ind = hier_ind_calc(Q_mat,N,layers)
 m=zeros(N);
 
 for ii = 1:(length(layers)-1)
@@ -6,5 +6,5 @@ for ii = 1:(length(layers)-1)
   m(k1,k2)=1; m(k2,k1)=1;
 end
 
-heir_ind = sum(sum(Q_mat.*m));
+hier_ind = sum(sum(Q_mat.*m));
 end
